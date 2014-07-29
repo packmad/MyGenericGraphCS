3674610149a589166c55205ad4130d557ad63f3a
SiX110 <simone.aonzo@gmail.com>
2014-07-29 11:16:54 +0200

    bbang

---
+++ MyGenericGraph/IGraph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericGraph
+{
+    interface IGraph<V, E> where E : IEdge<V, V>
+    {
+
+    }
+}
---
+++ MyGenericGraph/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericGraph
+{
+    class Edge : IEdge<int, float>
+    {
+        
+    }
+    class Graph : IGraph<Object, Number>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
